ff6a4801f9bcd48c748b9057a911403433f9de2e
SONG22 <annna0449@naver.com>
2020-07-25 12:21:40 +0900

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello express!");
+});
+
 app.listen(3060, () => {
   console.log("서버실행중");
 });
